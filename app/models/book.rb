e3a1c46218559e4a5a7b9af6193dd1b0ddb571c6
Naoto Taniguchi <ntaniguchi94@gmail.com>
2016-11-26 09:23:46 +0000

    アプリ暫定完成！

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
+    has_many :reviews
     validates :name, presence:true, length:{maximum:20}
     validates :price, presence:true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126032938) do
+ActiveRecord::Schema.define(version: 20161126082602) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20161126032938) do
     t.datetime "publish_date"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "book_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
